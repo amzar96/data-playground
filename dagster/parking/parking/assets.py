674897d0160928c8e523624a16aa73ced4bdd2d3
amzar <mohdamzar96@gmail.com>
2025-03-08 13:55:49 +0800

    update data dagster

---
+++ dagster/parking/parking/assets.py
@@ -0,0 +1,344 @@
+import dagster as dg
+from dagster_duckdb import DuckDBResource
+
+bronze_group_name = "bronze"
+silver_group_name = "silver"
+gold_group_name = "gold"
+base_silver_group_name = "base_silver"
+
+
+def create_fact_dim(
+    query, table_name, schema_name, duckdb: DuckDBResource
+) -> dg.MaterializeResult:
+    table_name = table_name
+    schema_name = base_silver_group_name
+
+    with duckdb.get_connection() as conn:
+        conn.execute(query)
+
+        preview_query = f"select * from {schema_name}.{table_name} limit 10"
+        preview_df = conn.execute(preview_query).fetchdf()
+
+        row_count = conn.execute(
+            f"select count(*) from {schema_name}.{table_name}"
+        ).fetchone()
+        count = row_count[0] if row_count else 0
+
+        return dg.MaterializeResult(
+            metadata={
+                "row_count": dg.MetadataValue.int(count),
+                "preview": dg.MetadataValue.md(preview_df.to_markdown(index=False)),
+            }
+        )
+
+
+def create_bronze_table(
+    table_name: str, duckdb: DuckDBResource
+) -> dg.MaterializeResult:
+    schema_name = bronze_group_name
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            f"""
+            create or replace table {schema_name}.{table_name} as (
+                select *, current_timestamp as etl_dt from read_csv_auto('data/{table_name}.csv')
+            )
+            """
+        )
+
+        preview_df = conn.execute(
+            f"select * from {schema_name}.{table_name} limit 10"
+        ).fetchdf()
+        count = conn.execute(
+            f"select count(*) from {schema_name}.{table_name}"
+        ).fetchone()[0]
+
+        return dg.MaterializeResult(
+            metadata={
+                "row_count": dg.MetadataValue.int(count),
+                "preview": dg.MetadataValue.md(preview_df.to_markdown(index=False)),
+            }
+        )
+
+
+def create_silver_table(
+    table_name: str, duckdb: DuckDBResource
+) -> dg.MaterializeResult:
+    schema_name = silver_group_name
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            f"""
+            create or replace table {schema_name}.{table_name} as (
+                with cte as (
+                    select * from {bronze_group_name}.{table_name}
+                )
+                select *, current_timestamp as etl_dt from cte
+            )
+            """
+        )
+
+        preview_df = conn.execute(
+            f"select * from {schema_name}.{table_name} limit 10"
+        ).fetchdf()
+        count = conn.execute(
+            f"select count(*) from {schema_name}.{table_name}"
+        ).fetchone()[0]
+
+        return dg.MaterializeResult(
+            metadata={
+                "row_count": dg.MetadataValue.int(count),
+                "preview": dg.MetadataValue.md(preview_df.to_markdown(index=False)),
+            }
+        )
+
+
+def create_missing_value_check(
+    table_name: str, column_name: str, duckdb: DuckDBResource
+) -> dg.MaterializeResult:
+    with duckdb.get_connection() as conn:
+        query_result = conn.execute(
+            f"""
+                select count(*) from {table_name}
+                where {column_name} is null
+                """
+        ).fetchone()
+
+        count = query_result[0] if query_result else 0
+        return dg.AssetCheckResult(
+            passed=count == 0, metadata={"missing column value": count}
+        )
+
+
+@dg.asset(compute_kind="duckdb", group_name=bronze_group_name)
+def bronze_cars(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_bronze_table("cars", duckdb)
+
+
+@dg.asset(compute_kind="duckdb", group_name=bronze_group_name)
+def bronze_customers(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_bronze_table("customers", duckdb)
+
+
+@dg.asset(compute_kind="duckdb", group_name=bronze_group_name)
+def bronze_parking_lots(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_bronze_table("parking_lots", duckdb)
+
+
+@dg.asset(compute_kind="duckdb", group_name=bronze_group_name)
+def bronze_transactions(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_bronze_table("transactions", duckdb)
+
+
+@dg.asset(compute_kind="duckdb", group_name=silver_group_name, deps=[bronze_cars])
+def silver_cars(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_silver_table("cars", duckdb)
+
+
+@dg.asset(compute_kind="duckdb", group_name=silver_group_name, deps=[bronze_customers])
+def silver_customers(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_silver_table("customers", duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb", group_name=silver_group_name, deps=[bronze_parking_lots]
+)
+def silver_parking_lots(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_silver_table("parking_lots", duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb", group_name=silver_group_name, deps=[bronze_transactions]
+)
+def silver_transactions(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    return create_silver_table("transactions", duckdb)
+
+
+@dg.asset_check(asset=silver_transactions)
+def silver_transactions_total_fare__missing_value(
+    duckdb: DuckDBResource,
+) -> dg.AssetCheckResult:
+    create_missing_value_check(
+        f"{silver_group_name}.transactions", "total_fare", duckdb
+    )
+
+
+@dg.asset_check(asset=silver_customers)
+def silver_customers_mobile_no__missing_value(
+    duckdb: DuckDBResource,
+) -> dg.AssetCheckResult:
+    create_missing_value_check(f"{silver_group_name}.customers", "mobile_no", duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb",
+    group_name=base_silver_group_name,
+    deps=[silver_customers],
+)
+def dim_customers(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    table_name = "dim_customers"
+    schema_name = base_silver_group_name
+
+    query = f"""
+            create or replace table {schema_name}.{table_name} as (
+            select 
+                base.id_no as customer_id,
+                base.name,
+                base.is_active,
+                base.mobile_no,
+                base.id_no,
+                base.country,
+                base.created_at,
+                base.updated_at,
+                current_timestamp as etl_dt
+            from {silver_group_name}.customers as base
+        )
+
+        """
+    return create_fact_dim(query, table_name, schema_name, duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb",
+    group_name=base_silver_group_name,
+    deps=[silver_parking_lots],
+)
+def dim_parking_lots(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    table_name = "dim_parking_lots"
+    schema_name = base_silver_group_name
+
+    query = f"""
+            create or replace table {schema_name}.{table_name} as (
+    select 
+        base.lot_id,
+        base.name,
+        base.address,
+        base.fare_per_hour,
+        base.created_at,
+        base.updated_at,
+        current_timestamp as etl_dt
+    from {silver_group_name}.parking_lots as base
+    )
+        """
+    return create_fact_dim(query, table_name, schema_name, duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb",
+    group_name=base_silver_group_name,
+    deps=[silver_cars],
+)
+def dim_cars(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    table_name = "dim_cars"
+    schema_name = base_silver_group_name
+
+    query = f"""
+        create or replace table {schema_name}.{table_name} as (
+    select 
+        no_plat as car_id,
+        customer_id,
+        current_timestamp as etl_dt
+    from {silver_group_name}.cars as base
+    )
+        """
+    return create_fact_dim(query, table_name, schema_name, duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb",
+    group_name=base_silver_group_name,
+    deps=[silver_transactions],
+)
+def dim_transactions(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    table_name = "dim_transactions"
+    schema_name = base_silver_group_name
+
+    query = f"""
+        create or replace table {schema_name}.{table_name} as (
+    select 
+        trans.id as transaction_id,
+        trans.total_fare,
+        trans.entry_time,
+        trans.out_time,
+        trans.customer_id,
+        trans.lot_id,
+        car.car_id as car_no_plat,
+        current_timestamp as etl_dt
+    from {silver_group_name}.transactions trans
+    left join {schema_name}.dim_cars car on car.car_id = trans.car_id
+    )
+        """
+    return create_fact_dim(query, table_name, schema_name, duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb",
+    group_name=base_silver_group_name,
+    deps=[dim_customers, dim_cars],
+)
+def fact_cars_customer(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    table_name = "fact_master_customers"
+    schema_name = base_silver_group_name
+
+    query = f"""
+            create or replace table {schema_name}.fact_master_customers as (
+                select 
+                    base.id_no,
+                    count(distinct car.no_plat) as total_cars,
+                    current_timestamp as etl_dt
+                from {silver_group_name}.customers as base
+                left join {silver_group_name}.cars car on car.customer_id = base.id_no
+                group by base.id_no
+            )
+        """
+    return create_fact_dim(query, table_name, schema_name, duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb",
+    group_name=base_silver_group_name,
+    deps=[dim_transactions, dim_customers],
+)
+def fact_transactions(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    table_name = "fact_transactions"
+    schema_name = base_silver_group_name
+
+    query = f"""
+            create or replace table {schema_name}.fact_transactions as (
+                select 
+                    trans.customer_id,
+                    trans.lot_id,
+                    car.car_id as car_no_plat,
+                    count(trans.transaction_id) as total_transactions,
+                    sum(trans.total_fare) as total_fare,
+                    avg(trans.total_fare) as avg_fare,
+                    max(trans.entry_time) as last_entry_time,
+                    min(trans.out_time) as first_out_time,
+                    current_timestamp as etl_dt
+                from {schema_name}.dim_transactions trans
+                left join {schema_name}.dim_cars car on car.car_id = trans.car_no_plat
+                group by 
+                    trans.customer_id, 
+                    trans.lot_id, 
+                    car.car_id
+            )
+        """
+    return create_fact_dim(query, table_name, schema_name, duckdb)
+
+
+@dg.asset(
+    compute_kind="duckdb",
+    group_name=gold_group_name,
+    deps=[fact_cars_customer, fact_transactions],
+)
+def gold_customer_report(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    table_name = "customer_report"
+    schema_name = base_silver_group_name
+
+    query = f"""
+        create or replace table {schema_name}.{table_name} as (
+            select 
+                trans.*,
+                current_timestamp as etl_dt
+            from {base_silver_group_name}.fact_transactions trans
+        )
+        """
+    return create_fact_dim(query, table_name, schema_name, duckdb)
---
+++ dagster/parking/parking/definitions.py
@@ -0,0 +1,47 @@
+import dagster as dg
+
+from dagster_duckdb import DuckDBResource
+from parking.assets import (
+    bronze_cars,
+    bronze_transactions,
+    bronze_parking_lots,
+    bronze_customers,
+    silver_cars,
+    silver_customers,
+    silver_parking_lots,
+    silver_transactions,
+    silver_transactions_total_fare__missing_value,
+    silver_customers_mobile_no__missing_value,
+    fact_cars_customer,
+    dim_cars,
+    dim_customers,
+    dim_transactions,
+    dim_parking_lots,
+    fact_transactions,
+    gold_customer_report,
+)
+
+defs = dg.Definitions(
+    assets=[
+        bronze_cars,
+        bronze_transactions,
+        bronze_parking_lots,
+        bronze_customers,
+        silver_cars,
+        silver_customers,
+        silver_parking_lots,
+        silver_transactions,
+        dim_cars,
+        dim_customers,
+        dim_transactions,
+        dim_parking_lots,
+        fact_cars_customer,
+        fact_transactions,
+        gold_customer_report,
+    ],
+    asset_checks=[
+        silver_transactions_total_fare__missing_value,
+        silver_customers_mobile_no__missing_value,
+    ],
+    resources={"duckdb": DuckDBResource(database="data/mydb.duckdb")},
+)
